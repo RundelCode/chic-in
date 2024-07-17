be045fc09e7a41e77549b8646516df0ef93eceb0
tamayo2 <juan.tamayo3@utp.edu.co>
2024-07-17 01:44:58 -0500

    Cambios de interfaz

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
   const [comments, setComments] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [hasActiveService, setHasActiveService] = useState(false);
+  const [selectedRole, setSelectedRole] = useState('clients');
 
   const { requestService, setServiceAsPending, pendingService, getActiveService, setUserId } = useService();
   const { user, loginToken } = useAuth();
@@ -96,86 +97,100 @@ export default function Home() {
             <Link href="/Profile" className={styles.profileLink}>Ir a mi perfil</Link>
           </div>
         ) : (
-          <form id="serviceForm" className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.formRow}>
-              <div className={styles.formInputContainer}>
-                <p>¿Qué servicio estás buscando?</p>
-                <select
-                  id="serviceType"
-                  className={styles.selectBox}
-                  value={serviceType}
-                  onChange={(e) => setServiceType(e.target.value)}
-                  required
-                >
-                  <option value="">Selecciona una opción</option>
-                  <option value="Lifting de pestañas">Lifting de pestañas</option>
-                  <option value="Uñas">Uñas</option>
-                </select>
-              </div>
-              <div className={styles.formInputContainer}>
-                <p>Fecha y hora</p>
-                <input
-                  id="dateAndTime"
-                  type="datetime-local"
-                  className={styles.formInput}
-                  value={dateTime}
-                  onChange={(e) => setDateTime(e.target.value)}
-                  required
-                />
-              </div>
-            </div>
+            <form id="serviceForm" className={styles.form} onSubmit={handleSubmit}>
+                <div className={styles.formRow}>
+                    <div className={styles.formInputContainer}>
+                        <p>¿Qué servicio estás buscando?</p>
+                        <select
+                            id="serviceType"
+                            className={styles.selectBox}
+                            value={serviceType}
+                            onChange={(e) => setServiceType(e.target.value)}
+                            required
+                        >
+                            <option value="">Selecciona una opción</option>
+                            <option value="Lifting de pestañas">Lifting de pestañas</option>
+                            <option value="Uñas">Uñas</option>
+                        </select>
+                    </div>
+                    <div className={styles.formInputContainer}>
+                        <p>Tecnica</p>
+                        <select
+                            id="serviceType"
+                            className={styles.selectBox}
+                            value={serviceType}
+                            onChange={(e) => setServiceType(e.target.value)}
+                            required
+                        >
+                            <option value="">Tradicional</option>
+                            <option value="Lifting de pestañas">Lifting de pestañas</option>
+                            <option value="Uñas">Uñas</option>
+                        </select>
+                    </div>
+                </div>
+                <div className={styles.roleSwitch}>
+                    <button
+                        className={`${styles.roleButton} ${selectedRole === 'clients' ? styles.selected : ''}`}
+                        onClick={() => setSelectedRole('clients')}>
+                        Manos
+                    </button>
+                    <button
+                        className={`${styles.roleButton} ${selectedRole === 'providers' ? styles.selected : ''}`}
+                        onClick={() => setSelectedRole('providers')}>
+                        Pies
+                    </button>
+                </div>
+                <div className={styles.formRow}>
+                    <div className={styles.formInputContainer}>
+                        <p>Precio (Tentativo)</p>
+                        <input
+                            id="price"
+                            type="number"
+                            className={`${styles.formInput} ${styles.noArrows}`}
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                            placeholder="$(COP)"
+                            required
+                        />
+                    </div>
+                    <div className={styles.formInputContainer}>
+                        <p>Medio de pago</p>
+                        <select className={styles.selectBox} required>
+                            <option value="">Selecciona una opción</option>
+                            <option value="Efectivo">Efectivo</option>
+                            <option value="Tarjeta de crédito">Tarjeta de crédito</option>
+                            <option value="Tarjeta de débito">Tarjeta de débito</option>
+                            <option value="Paypal">Paypal</option>
+                        </select>
+                    </div>
+                </div>
 
-            <div className={styles.formRow}>
-              <div className={styles.formInputContainer}>
-                <p>Precio (Tentativo)</p>
-                <input
-                  id="price"
-                  type="number"
-                  className={`${styles.formInput} ${styles.noArrows}`}
-                  value={price}
-                  onChange={(e) => setPrice(e.target.value)}
-                  placeholder="$(COP)"
-                  required
-                />
-              </div>
-              <div className={styles.formInputContainer}>
-                <p>Medio de pago</p>
-                <select className={styles.selectBox} required>
-                  <option value="">Selecciona una opción</option>
-                  <option value="Efectivo">Efectivo</option>
-                  <option value="Tarjeta de crédito">Tarjeta de crédito</option>
-                  <option value="Tarjeta de débito">Tarjeta de débito</option>
-                  <option value="Paypal">Paypal</option>
-                </select>
-              </div>
-            </div>
+                <div className={styles.formRow}>
+                    <div className={styles.formInputContainer}>
+                        <p>Teléfono</p>
+                        <input
+                            type="tel"
+                            className={`${styles.formInput} ${styles.noArrows}`}
+                            placeholder="(+57) Teléfono"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className={styles.formInputContainer}>
+                        <p>Ciudad</p>
+                        <input
+                            type="text"
+                            className={styles.formInput}
+                            placeholder="Ciudad"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
 
-            <div className={styles.formRow}>
-              <div className={styles.formInputContainer}>
-                <p>Teléfono</p>
-                <input
-                  type="tel"
-                  className={`${styles.formInput} ${styles.noArrows}`}
-                  placeholder="(+57) Teléfono"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  required
-                />
-              </div>
-              <div className={styles.formInputContainer}>
-                <p>Ciudad</p>
-                <input
-                  type="text"
-                  className={styles.formInput}
-                  placeholder="Ciudad"
-                  value={city}
-                  onChange={(e) => setCity(e.target.value)}
-                  required
-                />
-              </div>
-            </div>
-
-            <div className={styles.longFormRow}>
+                {/*<div className={styles.longFormRow}>
               <p>Dirección</p>
               <input
                 type="text"
@@ -185,33 +200,33 @@ export default function Home() {
                 onChange={(e) => setAddress(e.target.value)}
                 required
               />
-            </div>
+            </div>*/}
 
-            <div className={styles.longFormRow}>
-              <p>Comentarios</p>
-              <input
-                type="text"
-                className={styles.formInput}
-                placeholder="Cuéntanos los detalles de tu servicio"
-                value={comments}
-                onChange={(e) => setComments(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.formRow}>
-              <button type="submit" className={styles.sendButton}>Solicitar servicio</button>
-            </div>
-          </form>
+                <div className={styles.longFormRow}>
+                    <p>Comentarios</p>
+                    <input
+                        type="text"
+                        className={styles.formInput}
+                        placeholder="Cuéntanos los detalles de tu servicio"
+                        value={comments}
+                        onChange={(e) => setComments(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className={styles.formRow}>
+                    <button type="submit" className={styles.sendButton}>Solicitar servicio</button>
+                </div>
+            </form>
         )}
 
-        {showModal && (
-          <div id="confirmation" className={styles.confirmation}>
-            <div className={styles.ConfirmationModal}>
-              <h3 className={styles.confirmationText}>¡Servicio solicitado con éxito!</h3>
-              <Link className={styles.serviceListLink} href={'/Profile'}>Ir a la lista de servicios</Link>
-            </div>
-          </div>
-        )}
+          {showModal && (
+              <div id="confirmation" className={styles.confirmation}>
+                  <div className={styles.ConfirmationModal}>
+                      <h3 className={styles.confirmationText}>¡Servicio solicitado con éxito!</h3>
+                      <Link className={styles.serviceListLink} href={'/Profile'}>Ir a la lista de servicios</Link>
+                  </div>
+              </div>
+          )}
       </main>
     </TypeGuard>
   );
